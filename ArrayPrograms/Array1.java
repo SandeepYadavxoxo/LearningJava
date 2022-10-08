1448ee8ae8d1f62f6f02fedb09843fd7666f7293
Young Wizard <92782406+YoungWizard69@users.noreply.github.com>
2022-10-08 11:52:03 +0530

    Simple Array of Integers

--- ArrayPrograms/Array1.java
+++ ArrayPrograms/Array1.java
@@ -1,5 +1,21 @@
 package ArrayPrograms;
 
 public class Array1 {
+    public static void main(String args[]){
+        int x[] = new int[4];
+        int i;
+        x[0]=1;
+        x[1]=2;
+        x[2]=3;
+        x[3]=4;
+
+        System.out.println("Accessing Array position at 3 is :"+ " "+ x[3]);
+
+        for(i=0;i< x.length;i++) {
+            System.out.println("Printing Array in loop :"+ " "+x[i]);
+        }
+
+    }
 
 }
+
