819c69f4cf32427b471ea868cb78884dcddc04b1
Young Wizard <92782406+YoungWizard69@users.noreply.github.com>
2022-10-08 11:56:49 +0530

    Simple Array of Strings

---
+++ ArrayPrograms/ArrayofStrings.java
@@ -0,0 +1,15 @@
+package ArrayPrograms;
+
+public class ArrayofStrings {
+
+    public static void main(String args[]){
+
+
+        String s[];
+        s = new String[]{"Samdeep", "Mandeep", "Mohan", "Rohan", "Mayank","Santosh"};
+
+        System.out.println(s[5]);
+
+
+    }
+}
