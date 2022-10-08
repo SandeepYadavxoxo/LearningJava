a924d85e79c6c471923712412ad802a401a984ea
Young Wizard <92782406+YoungWizard69@users.noreply.github.com>
2022-10-08 12:49:18 +0530

    -

---
+++ ArrayPrograms/ArrayInputFromUser.java
@@ -0,0 +1,18 @@
+package ArrayPrograms;
+
+import java.util.Scanner;
+
+public class ArrayInputFromUser {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int arr[];                   //declaring array
+        arr =new int[5];            //allocating memory to array
+
+
+    }
+
+
+
+}
--- ArrayPrograms/ArrayofStrings.java
+++ ArrayPrograms/ArrayofStrings.java
@@ -5,9 +5,15 @@ public class ArrayofStrings {
     public static void main(String args[]){
 
 
+        String a[] = new String[3];
+        a = new String[]{"s","b","c","d","e"};
+
+
         String s[] ={"Sandeep", "Mandeep", "Mohan", "Rohan", "Mayank","Santosh"};
+
         int l= s.length;
-        System.out.println("total String length of Array [s] is :"+ " "+l);
+
+        System.out.println("total String length of Array [s] is :"+ " "+l+a);
 
         System.out.println("Printing Values in Array of s:");
         for (int i=0;i<s.length;i++){
