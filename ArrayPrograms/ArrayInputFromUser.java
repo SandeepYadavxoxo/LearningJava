a08fb0b0fbd2621cb3851e7d4466ae2690893a87
Young Wizard <92782406+YoungWizard69@users.noreply.github.com>
2022-10-08 13:05:50 +0530

    ArrayInputFromUser

--- ArrayPrograms/ArrayInputFromUser.java
+++ ArrayPrograms/ArrayInputFromUser.java
@@ -9,6 +9,17 @@ public class ArrayInputFromUser {
         Scanner sc = new Scanner(System.in);
         int arr[];                   //declaring array
         arr =new int[5];            //allocating memory to array
+        int i;
+        System.out.println("Enter Value in Array");
+        for (i=0;i<arr.length;i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Printing Values inside Array");
+
+        for (i=0;i<arr.length;i++){
+
+            System.out.println(arr[i]);
+        }
 
 
     }
