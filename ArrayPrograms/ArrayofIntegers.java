5cbaa9618e1b2ead281dc7c3051554f7a5cfe5af
Young Wizard <92782406+YoungWizard69@users.noreply.github.com>
2022-10-08 12:19:15 +0530

    added length property

--- ArrayPrograms/ArrayofIntegers.java
+++ ArrayPrograms/ArrayofIntegers.java
@@ -3,14 +3,15 @@ package ArrayPrograms;
 public class ArrayofIntegers {
     public static void main(String args[]){
         int x[] = new int[4];
+        int l;
+        l=x.length;
         int i;
         x[0]=1;
         x[1]=2;
         x[2]=3;
         x[3]=4;
 
-        System.out.println("Accessing Array position at 3 is :"+ " "+ x[2]);
-
+        System.out.println("length of array x is : "+" "+l);
         for(i=0;i< x.length;i++) {
             System.out.println("Printing Array in loop :"+ " "+x[i]);
         }
