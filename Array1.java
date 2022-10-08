c71369ea054e0fe0bbc8b86b1f2d3fcf5f3b337d
Young Wizard <92782406+YoungWizard69@users.noreply.github.com>
2022-10-08 11:36:09 +0530

    Simple Array of integer

---
+++ Array1.java
@@ -0,0 +1,16 @@
+public class Array1 {
+    public static void main(String args[]){
+        int x[] = new int[4];
+        int i;
+        x[0]=1;
+        x[1]=2;
+        x[2]=3;
+        x[3]=4;
+
+        for(i=0;i< x.length;i++) {
+            System.out.println(x[i]);
+        }
+
+    }
+
+}
