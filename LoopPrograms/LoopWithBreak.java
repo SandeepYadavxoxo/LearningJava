161f57d86a7e8bfc0bb633db31e9fc2a0a42b12f
SandeepYadavxoxo <sandeepyadavxoxo@gmail.com>
2022-11-01 12:42:36 +0530

    Break using for loop with if condition

--- LoopPrograms/LoopWithBreak.java
+++ LoopPrograms/LoopWithBreak.java
@@ -5,9 +5,16 @@ public class LoopWithBreak {
 
         int i;
         for (i = 0; i <= 10; i++) {
+//
+//            if (i == 3) {
+//                System.out.println("Loop is checking if i equal to 3");
+//                continue;
+//                //break;
+//            }
 
             if (i == 5) {
                 System.out.println("Breaking loop");
+                //return ;
                 break;
             }
             System.out.println(i);
